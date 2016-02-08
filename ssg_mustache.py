ac5e556c6624f0054e58c2d52dbb0bc3c34bb3b2
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-02-08 16:36:49 +0100

    First commit - v0.0.1

---
+++ ssg_mustache.py
@@ -0,0 +1,14 @@
+import pystache
+import ssg
+
+# v 0.0.1
+
+templates = {}
+
+templates = ssg.get_templates('.mustache')
+pystache_renderer = pystache.Renderer()
+site_config = ssg.add_urls_to_site_config(ssg.get_config(), templates)
+
+for template_key, template in templates.items():
+    env = {'global': site_config, 'local': template['config']}
+    ssg.create_html_file(template['html_path'], pystache_renderer.render_path(template['template_path'], env))
